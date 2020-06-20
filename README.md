1f796f966e01d8e2f87ed4e4be853d57df80add9
favanzo <f.avanzo@cinq.com.br>
2020-06-20 18:17:29 -0300

    Atualizando Readme

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# meetupTddAndroid
+# Meetup Tdd Android
+============================================================================
+
+Meetup com apresentando os conceitos sobre Teste Drive Development (TDD) as principais ferramentas utilizadas para contrução de testes de unidade automatizados em aplicativos mobile da plataforma android
+
+Conteudo
+--------
+O conteudo desse meetup foi baseda no Code Labs fornecido pelo google:
+*[Advanced Android in Kotlin: Welcome to the course]https://codelabs.developers.google.com/codelabs/advanced-android-kotlin-training-welcome/#1
--- android-testing/app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
+++ android-testing/app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
@@ -1,18 +1,19 @@
 package com.example.android.architecture.blueprints.todoapp.statistics
 
 import com.example.android.architecture.blueprints.todoapp.data.Task
-import org.junit.Assert.*
+import org.hamcrest.Matchers.`is`
+import org.junit.Assert.assertThat
 import org.junit.Test
 
 class StatisticsUtilsTest {
 
     @Test
-    fun getActiveAndCompletedStats_noCompleted_returnsHundredZero(){
+    fun getActiveAndCompletedStats_noCompleted_returnsHundredZero() {
         val tasks = listOf<Task>(
                 Task("title", "desc", isCompleted = false)
         )
         val result = getActiveAndCompletedStats(tasks)
-        assertEquals(result.completedTasksPercent, 0f)
-        assertEquals(result.activeTasksPercent, 100f)
+        assertThat(result.activeTasksPercent, `is`(100f))
+        assertThat(result.completedTasksPercent, `is`(0f))
     }
 }
